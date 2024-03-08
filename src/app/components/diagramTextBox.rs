5ec2242e9b3fcc00a3b933c3f7157daec4d14bba
erlingto <erling.tmte@gmail.com>
2024-03-08 17:23:55 +0100

    added buggy import function

--- src/app/components/diagramTextBox.rs
+++ src/app/components/diagramTextBox.rs
@@ -1,8 +1,7 @@
+use crate::app::dictionary::dict;
 use leptos::*;
 use leptos_use::core::{Position, Size};
 use log::debug;
-use styled::tracing::field::debug;
-use dict::{ Dict, DictIface };
 
 use crate::app::{
     structs::{
@@ -49,6 +48,107 @@ pub fn DiagramTextBox(
         setText(newText);
     });
 
+    let organizePositions = move |connections: Vec<RwSignal<ConnectionItem>>,
+                                  items: Vec<RwSignal<MoveBoxItem>>| {
+        let mut toRankDict = dict::<String, i32>::new();
+        let mut fromRankDict = dict::<String, i32>::new();
+
+        items.iter().for_each(|x| {
+            let key = x.get().key.to_string();
+            toRankDict.insert(key.clone(), 0);
+            fromRankDict.insert(key.clone(), 0);
+        });
+
+        connections.iter().for_each(|x| {
+            let from = x.get().from.get().key.to_string();
+            let to = x.get().to.get().key.to_string();
+            toRankDict.insert(to.clone(), toRankDict.get(to).unwrap() + 1);
+            fromRankDict.insert(from.clone(), fromRankDict.get(from.clone()).unwrap() + 1);
+        });
+
+        let mut toRankneighbourWeighted = dict::<String, i32>::new();
+        let mut fromRankneighbourWeighted = dict::<String, i32>::new();
+
+        items.iter().for_each(|x| {
+            let key = x.get().key.to_string();
+            toRankneighbourWeighted
+                .insert(key.clone(), toRankDict.get(key.clone()).unwrap().clone());
+            fromRankneighbourWeighted
+                .insert(key.clone(), fromRankDict.get(key.clone()).unwrap().clone());
+        });
+
+        connections.iter().for_each(|x| {
+            let from = x.get().from.get().key.to_string();
+            let to = x.get().to.get().key.to_string();
+            let toNeighbourValue = toRankDict.get(from.clone());
+            let fromNeighbourValue = fromRankDict.get(to.clone());
+            if (toNeighbourValue.is_some()) {
+                toRankneighbourWeighted.insert(
+                    to.clone(),
+                    *toNeighbourValue.unwrap() + toRankneighbourWeighted.get(to.clone()).unwrap(),
+                );
+            }
+            if (fromNeighbourValue.is_some()) {
+                fromRankneighbourWeighted.insert(
+                    from.clone(),
+                    *fromNeighbourValue.unwrap()
+                        + fromRankneighbourWeighted.get(from.clone()).unwrap(),
+                );
+            }
+        });
+
+        let mut toRankOverView = dict::<i32, Vec<String>>::new();
+        let mut fromRankOverView = dict::<i32, Vec<String>>::new();
+
+        toRankDict.into_iter().for_each(|x| {
+            if toRankOverView.get(x.1).is_none() {
+                toRankOverView.insert(x.1, vec![x.0]);
+            } else {
+                toRankOverView[x.1].push(x.0);
+            }
+        });
+
+        fromRankDict.into_iter().for_each(|x| {
+            if fromRankOverView.get(x.1).is_none() {
+                fromRankOverView.insert(x.1, vec![x.0]);
+            } else {
+                fromRankOverView[x.1].push(x.0);
+            }
+        });
+
+        let Ylevels = toRankOverView.clone().into_iter().len() as i32;
+        let mut Xlevels = vec![];
+        toRankOverView.clone().into_iter().for_each(|y| {
+            Xlevels.push(y.1.len() as i32);
+        });
+
+        items.iter().for_each(|item| {
+            debug!("{:?}", item.get().key);
+            debug!("{:?}", toRankneighbourWeighted.clone());
+            let rank = toRankneighbourWeighted
+                .clone()
+                .into_iter()
+                .find(|x| x.0 == item.get().key)
+                .unwrap()
+                .1;
+            let xRankList = toRankOverView
+                .clone()
+                .into_iter()
+                .find(|x| x.1.contains(&item.get().key))
+                .unwrap()
+                .1;
+            let xRank = xRankList.iter().position(|x| x == &item.get().key).unwrap() as i32;
+            let size = item.get().size.get();
+            debug!("{:?}", size.y);
+            let yPosition = (100 + (size.y + 145.0) as i32 * rank) as f64;
+            let xPosition = (500 + (size.x + 135.0) as i32 * xRank) as f64;
+            item.get().position.set(Position {
+                x: xPosition,
+                y: yPosition,
+            });
+        })
+    };
+
     let importDiagram = move || {
         let diagram = text.get();
         debug!("{:?}", diagram);
@@ -89,7 +189,11 @@ pub fn DiagramTextBox(
                 newItems.push(create_rw_signal(MoveBoxItem {
                     position: create_rw_signal(Position { x: 0.0, y: 0.0 }),
                     value: create_rw_signal(name),
-                    key: format!("{}:{}", importCount.get().to_string(), classCount.to_string()),
+                    key: format!(
+                        "{}:{}",
+                        importCount.get().to_string(),
+                        classCount.to_string()
+                    ),
                     attributes: create_rw_signal(att),
                     size: create_rw_signal(Position { x: 20.0, y: 20.0 }),
                 }));
@@ -110,9 +214,13 @@ pub fn DiagramTextBox(
                 let toItem = newItems.iter().find(|x| x.get().value.get() == to);
                 let fromItem = newItems.iter().find(|x| x.get().value.get() == from);
                 debug!("{:?}", toItem);
-                if (toItem.is_some() || fromItem.is_some()) {
+                if (toItem.is_some() && fromItem.is_some()) {
                     newConnections.push(create_rw_signal(ConnectionItem {
-                        key:format!("{}_{}", importCount.get().to_string(), connectionsCount.to_string()), 
+                        key: format!(
+                            "{}_{}",
+                            importCount.get().to_string(),
+                            connectionsCount.to_string()
+                        ),
                         from: *fromItem.unwrap(),
                         to: *toItem.unwrap(),
                     }));
@@ -123,85 +231,11 @@ pub fn DiagramTextBox(
         }
         debug!("{:?}", newConnections.len());
         debug!("{:?}", newItems.len());
+        organizePositions(newConnections.clone(), newItems.clone());
         setImportCount(importCount.get() + 1);
-        setItems(newItems); 
+        setItems(newItems);
         setConnections(newConnections);
     };
-
-    let organizePositions = move |connections: Vec<RwSignal<ConnectionItem>>, items: Vec<RwSignal<MoveBoxItem>>| {
-        let mut toRankDict = Dict::<String, i32>::new();
-        let mut fromRankDict = Dict::<String, i32>::new();
-        connections.iter().for_each(|x| {
-            let from = x.get().from.get();
-            let to = x.get().to.get();
-            if (toRankDict.get(to.key).is_none()){
-                toRankDict.add(to.key, 1);
-            }
-            else{
-                toRankDict.set(to.key, toRankDict.get(to.key).unwrap() + 1);
-            }
-            if (fromRankDict.get(from.key).is_none()) {
-                fromRankDict.add(from.key, 1);
-            }
-            else{
-                fromRankDict.set(from.key, fromRankDict.get(from.key).unwrap() + 1);
-            }
-        });
-
-        let mut toRankneighbourWeighted = Dict::<String, Dict<String, i32>>::new();
-        let mut fromRankneighbourWeighted = Dict::<String, Dict<String, i32>>::new();
-
-        connections.iter().for_each(|x| {
-            let from = x.get().from.get();
-            let to = x.get().to.get();
-            if (toRankneighbourWeighted.get(to.key).is_none()){
-                toRankneighbourWeighted.add(to.key, Dict::<String, i32>::new());
-            } 
-            else{
-                toRankneighbourWeighted.set(to.key, toRankDict.get(from.key).unwrap() + 1);
-            }
-            if (fromRankneighbourWeighted.get(from.key).is_none()) {
-                fromRankneighbourWeighted.add(from.key, fromRankDict.get(from.key).unwrap());
-            }
-            else{
-                fromRankneighbourWeighted.set(from.key, fromRankDict.get(to.key).unwrap() + 1);
-            }
-        });
-
-        let mut toRankOverView = Dict::<i32, Vec<String>>::new();
-        let mut fromRankOverView = Dict::<i32, Vec<String>>::new();
-
-        toRankDict.iter().for_each(|x| {
-            if toRankDict.get(x.1).is_none() {
-                toRankOverView.add(x.1, vec![x.0]);
-            } else {
-                toRankOverView.get(x.1).unwrap().push(x.0);
-            }
-        });
-
-        fromRankDict.iter().for_each(|x| {
-            if fromRankDict.get(x.1).is_none() {
-                toRankOverView.add(x.1, vec![x.0]);
-            } else {
-                toRankOverView.get(x.1).unwrap().push(x.0);
-            }
-        });
-
-        let Ylevels = toRankOverView.len();
-        let Xlevels = vec![];
-        toRankOverView.iter().for_each(|y| {
-            Xlevels.push(y.1.len());
-        });
-
-        items.iter().for_each(|item|){
-            let rank = toRankneighbourWeighted.iter().find(|x| x.get().key == to.key);
-            
-            let yPosition = 200.0 / Ylevels * rank;
-            let xPosition = xLevels[rank] * 200.0 / Xlevels[rank];
-        }
-
-
-    }
 
     view! {
         <div style="position: fixed; right: 2vw; width: 16vw; height: 50%;">
@@ -216,7 +250,7 @@ pub fn DiagramTextBox(
             </textarea>
             <TioButton
                 onClick=move || { importDiagram() }
-                text=Signal::derive(move || "Save Diagram".to_string())
+                text=Signal::derive(move || "Import Diagram".to_string())
                 style="".to_string()
             />
         </div>
