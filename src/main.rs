f0dc5a2ea7fc81bbd7a69a99cb108fcb107c84a9
erlingto <erling.tmte@gmail.com>
2024-06-03 01:57:51 +0200

    removed nonreactive zone

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,5 @@ fn main() {
     console_log::init_with_level(Level::Debug);
     console_error_panic_hook::set_once();
     debug!("Starting app...");
-    SpecialNonReactiveZone::enter();
     mount_to_body(|| view! { <App/> })
 }
